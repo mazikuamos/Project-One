9d2a79e6ceab4a2ecadefa87a0568871f4624dcf
mazikuamos <amosmaziku123@yahoo.com>
2023-01-17 08:54:40 +0300

    Update likes app and apply Generic Relationships

--- likes/models.py
+++ likes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class Likes(models.Model):
+class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
