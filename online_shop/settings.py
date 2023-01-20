075b4d498a67e1dec013b59b4014edfb0fd11958
mazikuamos <amosmaziku123@yahoo.com>
2023-01-21 01:06:32 +0300

    Setting mysql database

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'online_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'online_shop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Amos123J'
     }
 }
 
